d6fe8a242c198b0cc09648d166a9a63c85905061
pedro.morias <88340056+Pedros-Morais@users.noreply.github.com>
2025-08-11 12:08:50 -0300

    fix build and datas

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from './components/header';
 import Hero from './components/hero';
 import About from './components/about';
+import Journey from './components/journey';
 import Projects from './components/projects';
 import StackBox from './components/stackBox';
 import FinalCta from './components/finalCta';
@@ -11,6 +12,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
+      <Journey />
       <Projects />
       <StackBox />
       <FinalCta />
